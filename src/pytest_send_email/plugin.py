e0bad4f08048c06920c0c739e6add08baeafa12d
unknown <dcy4725@163.com>
2024-05-30 17:53:20 +0800

    Signed-off-by: unknown <dcy4725@163.com>

--- src/pytest_send_email/plugin.py
+++ src/pytest_send_email/plugin.py
@@ -1,10 +1,12 @@
+import os
 from datetime import datetime
 
+
 def pytest_configure():
-    '''用例开始前执行'''
+    """用例开始前执行"""
     print(f"{datetime.now()} pytest开始执行")
 
 
 def pytest_unconfigure():
-    '''用例结束后执行'''
+    """用例结束后执行"""
     print(f"{datetime.now()} pytest结束执行")
